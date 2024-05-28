2f1f4449d77144916f79dd3e1a08c2e51c4297db
theertha120 <141700972+theertha120@users.noreply.github.com>
2024-05-28 14:48:33 +0300

    Update App.js

--- artistically/src/App.js
+++ artistically/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import BackgroundSlider from "./bgm";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <BackgroundSlider />
+        <img id="LOGO" src="LOGO.jpg" />
+        <h1 className="welcome">Welcome to ArtisticAlly!</h1>
+        <button className="button">Start exploring➡️</button>
+
+        {/* Add other components or content here */}
+      </div>
+    </>
   );
-}
+};
+
+export default App;
+
 
 export default App;
