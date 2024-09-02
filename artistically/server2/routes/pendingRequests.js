708cb080c5a17ed39161e4bca0fe9cab8bb66c60
theertha120 <141700972+theertha120@users.noreply.github.com>
2024-09-02 20:34:51 +0800

    Update and rename pendingRequests.js to expenditures.js

--- artistically/server2/routes/pendingRequests.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const pendingRequestsController = require('../controllers/pendingRequestsController');
-
-router.get('/:userEmail', pendingRequestsController.getPendingRequests);
-
-module.exports = router;
