30f671a71c31f1b0d5f1e47a9a04616f83f3f350
theertha120 <141700972+theertha120@users.noreply.github.com>
2024-09-02 23:12:17 +0800

    Update expenditureController.js

--- artistically/server2/controllers/expenditureController.js
+++ artistically/server2/controllers/expenditureController.js
@@ -1,18 +1,45 @@
-const db = require('../db');
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'test',
+  database: 'artistically'
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+    return;
+  }
+  console.log('Connected to the database!');
+});
 
 const getExpenditures = (req, res) => {
-  db.query('SELECT * FROM expenditures', (err, results) => {
-    if (err) throw err;
+  connection.query('SELECT * FROM expenditures', (err, results) => {
+    if (err) {
+      console.error('Error fetching expenditures:', err);
+      res.status(500).send('Server error');
+      return;
+    }
     res.json(results);
   });
 };
 
 const addExpenditure = (req, res) => {
   const { category, name, cost } = req.body;
-  db.query('INSERT INTO expenditures (category, name, cost) VALUES (?, ?, ?)', [category, name, cost], (err) => {
-    if (err) throw err;
-    res.status(201).send('Expenditure added');
-  });
+  connection.query(
+    'INSERT INTO expenditures (category, name, cost) VALUES (?, ?, ?)', 
+    [category, name, cost], 
+    (err) => {
+      if (err) {
+        console.error('Error adding expenditure:', err);
+        res.status(500).send('Server error');
+        return;
+      }
+      res.status(201).send('Expenditure added');
+    }
+  );
 };
 
 module.exports = { getExpenditures, addExpenditure };
