b3ba464d5a8d1886782fb53061c1c414dc53fa23
theertha120 <141700972+theertha120@users.noreply.github.com>
2024-09-02 23:13:40 +0800

    Update and rename server.js to index.js

--- artistically/server2/index.js
+++ artistically/server2/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const expenditureRoutes = require('./routes/expenditures');
+const expenditureRoutes = require('./routes/expenditures'); // Ensure this path is correct
 
 const app = express();
 app.use(cors());
